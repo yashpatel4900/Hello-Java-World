0c5fdf96d1b5e6fc8ee56fcb012616bdf44ae4a1
Yash Patel <yashpatel49000@gmail.com>
2020-07-02 18:06:31 +0530

    Conditions Added

--- Mortgage_Calculator.java
+++ Mortgage_Calculator.java
@@ -3,22 +3,46 @@ import java.lang.*;
 import java.text.NumberFormat;
 
 public class Mortgage_Calculator {
-    public static void main( String args[]){
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Principal: ");
-        double principal = sc.nextDouble();
-        System.out.println("Annual Interest Rate: ");
-        double interest = sc.nextDouble();
-        double month_interest = interest / (100*12);
-        System.out.println("Period Years: ");
-        double time = sc.nextDouble();
-        time = time*12;
 
-        double mortgage = (principal*(month_interest*Math.pow((1 + month_interest), time))) / (Math.pow((1 + month_interest), time) - 1);
+        double principal;
+        while (true) {
+            System.out.println("Principal ($1k- $1M): ");
+            principal = sc.nextDouble();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter valid value");
+        }
+
+        double interest;
+        double month_interest;
+        while (true) {
+            System.out.println("Annual Interest Rate: ");
+            interest = sc.nextDouble();
+            month_interest = interest / (100 * 12);
+            if (interest >= 1 && interest <= 30)
+                break;
+            System.out.println("Enter valid value");
+        }
+
+        double time;
+        while (true) {
+            System.out.println("Period Years: ");
+            time = sc.nextDouble();
+            if (time >= 1 && time <= 30)
+                break;
+            System.out.println("Enter valid value");
+        }
         
+        time = time * 12;
+
+        double mortgage = (principal * (month_interest * Math.pow((1 + month_interest), time)))
+                / (Math.pow((1 + month_interest), time) - 1);
+
         NumberFormat curreny = NumberFormat.getCurrencyInstance();
         String result = curreny.format(mortgage);
-        System.out.println("Mortgage: "+ result);
+        System.out.println("Mortgage: " + result);
     }
-    
+
 }
